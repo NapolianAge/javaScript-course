3c4cd5b1394333abfed626af1c7f65dd1797619e
Dmitry <dmitry@example.com>
2026-02-20 00:57:26 -0800

    Lesson 2

--- lessons/lesson-01/main.js
+++ lessons/lesson-01/main.js
@@ -4,3 +4,14 @@ let counter = 150;
   } else {
   console.log(counter);
   }
+let a = 68;
+ let b = 99; 
+  if (a + b > 15 ) {
+ console.log("da");
+  } else {
+    console.log("no");
+  }
+
+  let goal = "7"
+  let ball = 7;
+      console.log(goal === ball); 
